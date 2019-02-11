22affad984742491314c318ccbfa6e6f8b321abe
Alexandra Lopez Obando <alexandralopezobando@MacBook-Pro-de-Alexandra.local>
2019-02-10 20:01:28 -0500

    cambios Alex

--- cinemapp/urls.py
+++ cinemapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('apps.accounts.urls')),
     path('sucursales/', include('apps.sucursales.urls')),
     path('peliculas/', include('apps.peliculas.urls')),
+    path('salas/', include('apps.salas.urls')),
     path('select2/', include('django_select2.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
